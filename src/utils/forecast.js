3be9073bb0a53544f728243ad360e59403085772
Josh Rogen <rogenjh@gmail.com>
2020-04-09 14:53:30 -0400

    Modified what the forecast shows

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location', undefined);
         } else {
             callback(undefined, body.current.weather_descriptions[0] +` It is currently ` + body.current.temperature + ` degrees out. There is a ` + 
-            body.current.precip + ` chance that it will rain today.`);
+            body.current.precip + `% chance that it will rain today. The windspeed is ` + body.current.wind_speed + ` in the ` + body.current.wind_dir + ` direction.`);
         }
     })
 }
